98e200ada31212e9b838b985112f8e9d8c222f32
Toke Faurby <Tokeee@gmail.com>
2017-04-13 11:36:54 +0100

    Don't check certificate

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 savedir="kitti_data"
 mkdir -p -- "$savedir"
-wget https://www.dropbox.com/s/rpwlnn6j39jjme4/kitti_data.zip?dl=0 -O $savedir/prednet_kitti_data.zip
+wget https://www.dropbox.com/s/rpwlnn6j39jjme4/kitti_data.zip?dl=0 -O $savedir/prednet_kitti_data.zip --no-check-certificate
 unzip $savedir/prednet_kitti_data.zip -d $savedir
